7adeb8ab089ae7bb42d2a5f711b21457f9b3511f
carpon02 <abubakarmukhtar2002@gmail.com>
2025-04-16 00:07:19 +0100

    assets files added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-gray-400'>
-      Navabr
+      
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#303F60",
+        secondary: "#CCE5FF",
+        accent: "#9333EA",
+        neutral: "#374151",
+        success: "#36EA88",
+        warning: "#FBBD23",
+        error: "#FD0025",
+      },
+    },
   },
   plugins: [],
 }
